5d7d6f5dd1f3f5e1841ccf11a9fe58852da26fb3
Judith Mumbua <munangaajudith@gmail.com>
2026-03-04 22:43:26 +0300

    Initial commit - Task Manager models

---
+++ TaskManager/myApp/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class User(AbstractUser):
+    role_choices = (
+        ('MANAGER','Manager'),
+        ('MEMBER','Member')
+    )
+    role = models.CharField(max_length=20,choices=role_choices, default='MEMBER')
+
+class Task(models.Model):
+    status_choices = (
+        ('PENDING','Pending'),
+        ('IN_PROGRESS','In Progress'),
+        ('DONE','Done'),
+    )
+    Priority_choices = (
+        ('HIGH', 'High'),
+        ('MEDIUM', 'Medium'),
+        ('LOW', 'Low'),
+    )
+    title = models.CharField(max_length=100)
+    description = models.TextField
+    status = models.CharField(max_length=20,choices=status_choices,default='Pending')
+    priority = models.CharField(max_length=20, choices=Priority_choices, )
+    due_date = models.DateTimeField()
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,related_name='tasks_created')
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks_assigned')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
